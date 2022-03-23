82aa05dadcc37865ffa5c68e1b39a55579ac44c1
dorian <baikunz@gmail.com>
2022-03-23 23:41:03 +0100

    feat[chezmoi]: Update dotfiles remote to ssh

--- installers/1_chezmoi.sh
+++ installers/1_chezmoi.sh
@@ -10,3 +10,6 @@ chezmoi init --apply https://github.com/baikunz/dotfiles.git -S $HOME/.dotfiles
 # Install znap
 git clone --depth 1 -- https://github.com/marlonrichert/zsh-snap.git $HOME/.config/zsh/modules/zsh-snap
 source $HOME/.config/zsh/modules/zsh-snap/install.zsh
+
+# Update dotfiles repo url to use ssh since we now have synced the ssh key
+chezmoi git -- remote set-url origin git@github.com:baikunz/dotfiles.git
