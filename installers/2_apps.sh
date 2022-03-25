aed71b5605bdfa8ba4e53ee3fed61c5937ed7464
dorian <baikunz@gmail.com>
2022-03-25 16:09:47 +0100

    feat[bettertouchtool]: Use custom cask for installing btt

--- installers/2_apps.sh
+++ installers/2_apps.sh
@@ -4,6 +4,7 @@ tap "brewkunz/taps", "git@github.com:brewkunz/taps.git"
 cask "brewkunz-alfred"
 cask "brewkunz-bartender"
 cask "brewkunz-istat-menus"
+cask "brewkunz-bettertouchtool"
 
 # Dev apps
 cask "brewkunz-postico"
